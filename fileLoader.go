4719b55251e4169da0f13b054f64b91343edef0f
sahil <sahil.dadia@hertz.net>
2024-03-12 20:49:29 +0000

    Added ls function to fileLoader.go

--- fileLoader.go
+++ fileLoader.go
@@ -3,6 +3,7 @@ package GoHelperFunctions
 import (
 	"bufio"
 	"fmt"
+	"log/slog"
 	"os"
 	"strings"
 )
@@ -26,4 +27,26 @@ func ReadFile(fileName string) []string {
 	}
 
 	return lines
+}
+
+func ls(directoryPath string) ([]string, []string, error) {
+
+	var files = []string{}
+	var directories = []string{}
+
+	directoryEntries, err := os.ReadDir(directoryPath)
+	if err != nil {
+		slog.Error("Error listing in directory : %v. Error is %v\n", directoryPath, err)
+		return nil, nil, err
+	}
+
+	for _, entry := range directoryEntries {
+		if entry.IsDir() {
+			directories = append(directories, entry.Name())
+		} else {
+			files = append(files, entry.Name())
+		}
+	}
+
+	return files, directories, nil
 }
