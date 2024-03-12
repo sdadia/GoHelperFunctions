2f6230f820783c0e445439ed85b3b684d809b249
sahil <sahil.dadia@hertz.net>
2024-03-12 21:01:44 +0000

    Changed read file function to read bytes

--- fileLoader.go
+++ fileLoader.go
@@ -1,32 +1,19 @@
 package GoHelperFunctions
 
 import (
-	"bufio"
-	"fmt"
 	"log/slog"
 	"os"
-	"strings"
 )
 
-func ReadFile(fileName string) []string {
+func ReadFile(fileName string) (string, error) {
 
-	file, err := os.Open(fileName)
+	bytes, err := os.ReadFile(fileName)
 	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Successfully Opened file")
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		var newLine = scanner.Text()
-		ans := strings.Split(newLine, "Search :")
-		println(ans)
-		os.Exit(-1)
+		return "", err
 	}
 
-	return lines
+	return string(bytes), nil
+
 }
 
 func ls(directoryPath string) ([]string, []string, error) {
